63b8e9e839bc13dea116390c7a63e2b69aa8312c
bravovo <bogdan.nyschyi@gmail.com>
2024-02-19 22:19:31 +0200

    Edited password fields behavior

--- src/main/java/org/example/hotelmanager/controllers/RegisterFormController.java
+++ src/main/java/org/example/hotelmanager/controllers/RegisterFormController.java
@@ -4,6 +4,7 @@ import javafx.animation.FadeTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import javafx.util.Duration;
 
 public class RegisterFormController {
@@ -18,13 +19,10 @@ public class RegisterFormController {
     private Label pass_match_label_admin;
 
     @FXML
-    private PasswordField pass_field_admin;
+    private PasswordField admin_pass;
 
     @FXML
     private PasswordField pass_field_confirm_admin;
-
-    @FXML
-    private Button create_acc_btn;
 
     @FXML
     private Label pass_match_label;
@@ -46,27 +44,43 @@ public class RegisterFormController {
             pass_text.setText(pass_field.getText());
             pass_text.setVisible(true);
             pass_field.setVisible(false);
-        }else{
-            pass_field.setText(pass_text.getText());
-            pass_text.setVisible(false);
-            pass_field.setVisible(true);
+            return;
         }
+        pass_field.setText(pass_text.getText());
+        pass_field.setVisible(true);
+        pass_text.setVisible(false);
     }
 
     public void showAdminPasswordClick(ActionEvent event){
         if(show_admin_pass.isSelected()){
-            admin_pass_text.setText(pass_field_admin.getText());
+            admin_pass_text.setText(admin_pass.getText());
             admin_pass_text.setVisible(true);
-            pass_field_admin.setVisible(false);
-        }else{
-            pass_field_admin.setText(admin_pass_text.getText());
-            admin_pass_text.setVisible(false);
-            pass_field_admin.setVisible(true);
+            admin_pass.setVisible(false);
+            admin_pass_text.setEditable(false);
+            return;
         }
+        admin_pass_text.setEditable(true);
+        admin_pass.setText(admin_pass_text.getText());
+        admin_pass.setVisible(true);
+        admin_pass_text.setVisible(false);
     }
 
-    // TODO: Додати додаткову перевірку схожості паролів, коли пароль зверху показується.
-    //  Протестувати найбільше можливих випадків
+    public void editPass(MouseEvent event){
+        pass_field.setText(pass_text.getText());
+        pass_field.setVisible(true);
+        pass_text.setVisible(false);
+        show_pass_check.setSelected(false);
+        pass_field.requestFocus();
+    }
+
+    public void editAdminPass(MouseEvent event){
+        admin_pass.setText(admin_pass_text.getText());
+        admin_pass.setVisible(true);
+        admin_pass_text.setVisible(false);
+        show_admin_pass.setSelected(false);
+        admin_pass.requestFocus();
+    }
+
     public void createAccountClick(ActionEvent event){
         if(!pass_field.getText().equals(pass_field_confirm.getText())){
             pass_match_label.setVisible(true);
@@ -78,7 +92,7 @@ public class RegisterFormController {
     }
 
     public void createHotelClick(ActionEvent event){
-        if(!pass_field_admin.getText().equals(pass_field_confirm_admin.getText())){
+        if(!admin_pass.getText().equals(pass_field_confirm_admin.getText())){
             pass_match_label_admin.setVisible(true);
             FadeTransition fadeOutTransition = new FadeTransition(Duration.millis(2500), pass_match_label_admin);
             fadeOutTransition.setFromValue(1.0);
